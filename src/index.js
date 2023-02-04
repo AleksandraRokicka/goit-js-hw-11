5b596807bd93c73fbb89efeec9b79a852039bca5
AleksandraRokicka <rokicka.ola@gmail.com>
2023-02-04 16:47:16 +0100

    add css, and change js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import './css/styles.css';
+
+const url = 'https://pixabay.com/api/';
 
 const form = document.querySelector('.search-form');
-const input = document.querySelector('[name="searchQuery"]');
 const gallery = document.querySelector('.gallery');
 const searchButton = document.querySelector('button[type="submit"]');
 const loadingButton = document.querySelector('.load-more');
-
-let value = '';
 
 const perPage = 40;
 let currentPage = 1;
@@ -22,67 +23,67 @@ const dataFromApi = {
 };
 const { key, image_type, orientation, safesearch, lang, per_page } =
   dataFromApi;
-const url = 'https://pixabay.com/api/';
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'title',
+  close: true,
+  nav: true,
+  escKey: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 
 async function fetchImages() {
-    const urlApi = `https://pixabay.com/api/?key=${key}&q=${value}&image_type=${image_type}&orientation=${orientation}&safe_search=${safesearch}&lang=${lang}&per_page=${per_page}&page=${currentPage}`;
-    try {
-        const response = await axios.get(urlApi);
-        console.log(response);
-        const images = response.data.hits;
-        console.log(images);
-       
-        gallery.insertAdjacentHTML(
-          'beforeend',
-          images
-            .map(
-              el => `<div class="photo-card"><img src="${el.webformatURL} alt="${el.tags}" loading="lazy"/> <div class="info"><p class="info-item"><b>Likes: ${el.likes}</b></p><p class="info-item"><b>Views: ${el.views}</b></p><p class="info-item">
+  const input = document.querySelector("input[name='searchQuery']");
+  const searchTerm = input.value;
+  const urlApi = `https://pixabay.com/api/?key=${key}&q=${searchTerm}&image_type=${image_type}&orientation=${orientation}&safe_search=${safesearch}&lang=${lang}&per_page=${per_page}&page=${currentPage}`;
+  try {
+    let value = '';
+    const response = await axios.get(urlApi);
+    console.log(response);
+    const images = response.data.hits;
+    console.log(images);
+
+   gallery.insertAdjacentHTML(
+     'beforeend',
+     images
+       .map(
+         el => `<div class="photo-card"><img src="${el.webformatURL} alt="${el.tags}" loading="lazy"/> <div class="info"><p class="info-item"><b>Likes: ${el.likes}</b></p><p class="info-item"><b>Views: ${el.views}</b></p><p class="info-item">
       <b>Comments: ${el.comments}</b></p><p class="info-item"><b>Downloads: ${el.downloads}</b></p></div></div>`
-            )
-            .join('')
-        );
+       )
+       .join('')
+   );
 
-        // if (images.length === 0) {
-        // Notiflix.Notify.failure(
-        //   'Sorry, there are no images matching your search query. Please try again.');
-        // return;
-        // }
-        // if (images.length < images.per_page) {
-        //   Notiflix.Notify.warning(
-        //     "We're sorry, but you've reached the end of search results."
-        //   );
-        // }
+    if (images.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    if (images.length < images.perPage) {
+      Notiflix.Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
 
+    currentPage += 1;
   } catch (error) {
     console.error(error);
   }
 }
-fetchImages();
 
+searchButton.addEventListener('click', fetchImages);
+loadingButton.addEventListener('click', fetchImages);
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  currentPage = 1;
+  gallery.innerHTML = '';
+  fetchImages();
+});
 
-/* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>; */
-
-// webformatURL - link do małego obrazka.
-// largeImageURL - link do dużego obrazka.
-// tags - wiersz z opisem obrazka. Będzie pasować do atrybutu alt.
-// likes - liczba lajków.
-// views - liczba wyświetleń.
-// comments - liczba komentarzy.
-// downloads - liczba pobrań
+// function clearImageGallery() {
+//   gallery.innerHTML = '';
+// }
