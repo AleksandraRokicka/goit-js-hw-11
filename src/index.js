d29830634c3e3fbe85df3dd67bf27710473089de
AleksandraRokicka <rokicka.ola@gmail.com>
2023-02-04 17:09:48 +0100

    css

--- src/index.js
+++ src/index.js
@@ -51,8 +51,17 @@ async function fetchImages() {
      'beforeend',
      images
        .map(
-         el => `<div class="photo-card"><img src="${el.webformatURL} alt="${el.tags}" loading="lazy"/> <div class="info"><p class="info-item"><b>Likes: ${el.likes}</b></p><p class="info-item"><b>Views: ${el.views}</b></p><p class="info-item">
-      <b>Comments: ${el.comments}</b></p><p class="info-item"><b>Downloads: ${el.downloads}</b></p></div></div>`
+         el => `<div class="photo-card">
+         <img src="${el.webformatURL} alt="${el.tags}" loading="lazy"/> 
+         <div class="info">
+         <p class="info-item">
+         <b>Likes: ${el.likes}</b></p>
+         <p class="info-item">
+         <b>Views: ${el.views}</b></p>
+         <p class="info-item">
+         <b>Comments: ${el.comments}</b></p>
+        <p class="info-item">
+         <b>Downloads: ${el.downloads}</b></p></div></div>`
        )
        .join('')
    );
